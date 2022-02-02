f4a38912b6b3da50ed6bccd85b53a7fa8687ec40
DougSilvera <shortxjdoug@gmail.com>
2022-02-02 15:33:09 -0500

    list journals and journal details page built

--- src/components/journals/journalList.js
+++ src/components/journals/journalList.js
@@ -1,15 +1,54 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import JournalRepository from "../../repositories/JournalRepository";
+import { formattedDate } from "../Settings";
 
 export default () => {
-    const journalId = 1
-    return (
+  const [journals, setJournals] = useState([]);
+  const [reset, syncList] = useState([])
+  
+
+  useEffect(() => {
+    JournalRepository.getUserJournals().then((data) => {
+      setJournals(data);
+    });
+  }, [reset]);
+  const killJournal = (id) => {
+      JournalRepository.deleteJournal(id)
+      .then(() => {
+          syncList(reset + 1)
+      })
+  } 
+  
+  return (
     <>
-    <div>this is where the journal list will go</div>
-    <Link to={`/journalList/editJournal`}><button>edit journal</button></Link>
-    <Link to={`/journalList/journalDetail`}><button>journal detail</button></Link>
+      <div className="journals">
+        <ul className="journals-list">
+          {journals.map((journal) => {
+            return (
+              <li key={journal.id}>
+                Date Range: {formattedDate(journal.startDate)} through{" "}
+                {formattedDate(journal.endDate)} reflection:{" "}
+                {journal.reflection}{" "}
+                <Link to={`/journalList/editJournal/${journal.id}`}>
+                  <button>edit journal</button>
+                </Link>
+                <Link to={`/journalList/journalDetail/${journal.id}`}>
+                  <button>journal detail</button>
+                </Link>
+                <button
+                  id={journal.id}
+                  onClick={(evt) => {
+                   killJournal(evt.target.id);
+                  }}
+                >
+                  Delete Journal
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
-
-
-    )
-}
+  );
+};
